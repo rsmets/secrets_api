a40eceb4e55dc2b8c70a9aaf293088c27bf6470c
Ray Smets <rayjsmets@gmail.com>
2015-05-03 18:32:54 -0700

    added show action for users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ SecretApi::Application.routes.draw do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
+      resources :users, :only => [:show]
     end
   end
 end
